e20cfaab8c93abfba0fd396ef90c79a3ad49b273
Bogdan Koshtura <101816501+BogdanKoshtura@users.noreply.github.com>
2023-04-15 15:33:50 +0300

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon',
         'USER': 'root',
-        'PASSWORD': 'No name998',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
